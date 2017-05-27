c2b2b513b49583458bb0563efc5cb3396a4de943
tranvictor <vu.tran54@gmail.com>
2017-05-28 00:50:12 +0700

    fix test

--- ethereum/test_persistent_storage.go
+++ ethereum/test_persistent_storage.go
@@ -8,6 +8,10 @@ func (self *testPersistentStorage) Persist(data interface{}, id string) error {
 func (self *testPersistentStorage) Load(data interface{}, id string) (interface{}, error) {
 	if id == ACTIVE_SHARE_FILE {
 		return &map[string]gobShare{}, nil
+	} else if id == ACTIVE_CLAIM_FILE {
+		return &gobClaims{}, nil
+	} else if id == OPEN_CLAIM_FILE {
+		return &gobClaims{}, nil
 	}
 	return nil, nil
 }
