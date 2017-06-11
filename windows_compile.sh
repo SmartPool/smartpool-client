9f4d707a3fb54cd4e3e61281f575c4e50ba0565d
Ubuntu <victor@Server2.dckt3mn2jjxetg5ai0jdvtwuma.ix.internal.cloudapp.net>
2017-06-11 17:23:59 +0000

    prebuilt for windows

---
+++ windows_compile.sh
@@ -0,0 +1,24 @@
+mustrun() {
+   "$@"
+   if [ $? != 0 ]; then
+      printf "Error when executing command: '$*'\n"
+      exit $ERROR_CODE
+   fi
+}
+
+export CC=x86_64-w64-mingw32-gcc
+export CXX=x86_64-w64-mingw32-g++
+export CGO_ENABLED=1
+export GOOS=windows
+export GOARCH=amd64
+
+echo "Install dependencies..."
+mustrun build/env.sh go get -v golang.org/x/crypto/ssh/terminal
+mustrun build/env.sh go get -v gopkg.in/urfave/cli.v1
+mustrun build/env.sh go get -v github.com/bmizerany/pat
+mustrun build/env.sh go get -v github.com/mitchellh/go-homedir
+mustrun build/env.sh go get -v golang.org/x/net/context
+mustrun build/env.sh go get -v github.com/gorilla/websocket
+echo "Compiling SmartPool client for Windows..."
+mustrun build/env.sh go build -o smartpool.exe cmd/ropsten/ropsten.go
+echo "Done. You can run SmartPool by ./smartpool --help"
