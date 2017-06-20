1135b72a603387cd40012a948cc49ad14c70a1f9
tranvictor <vu.tran54@gmail.com>
2017-06-20 18:11:36 +0700

    fix test

--- protocol/test_rig.go
+++ protocol/test_rig.go
@@ -4,9 +4,13 @@ type testRig struct {
 }
 
 func (r *testRig) ID() string {
-	return "rig"
+	return "rig-127.0.0.1"
 }
 
 func (r *testRig) IP() string {
 	return "127.0.0.1"
+}
+
+func (r *testRig) Name() string {
+	return "rig"
 }
